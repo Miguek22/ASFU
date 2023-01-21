466e835c006bc8a1656d04ad469a489a91162f10
N7Softwares <n7softwares@gmail.com>
2023-01-21 11:37:08 -0300

    mejoras

--- js/funciones.js
+++ js/funciones.js
@@ -1,26 +1,22 @@
-let filter_btn = document.querySelectorAll('.filter-btn');
-let tab_items = document.querySelectorAll('.tab-item');
+var tabButtons=document.querySelectorAll(".contenedor-tab .botones-tab button");
+var tabPanels=document.querySelectorAll(".contenedor-tab .tabPanel");
 
-for (let i = 0; i < filter_btn.length; i++) {
-  filter_btn[i].addEventListener('click', function () {
-    for (let j = 0; j < filter_btn.length; j++) {
-      filter_btn[j].classList.remove('active');
-    }
-    let select_tab = filter_btn[i].getAttribute('data-tab');
-    filter_btn[i].classList.add('active');
-    for (let t = 0; t < tab_items.length; t++) {
-      document.querySelector('.tab-filter-item-container').style.height =
-        tab_items[t].scrollHeight + 'px';
-      if (tab_items[t].classList.contains(select_tab)) {
-        tab_items[t].classList.add('select_tab');
-      } else {
-        tab_items[t].classList.remove('select_tab');
-      }
-    }
-  });
+function showPanel(panelIndex,colorCode) {
+    tabButtons.forEach(function(node){
+        node.style.backgroundColor="";
+        node.style.color="";
+    });
+    tabButtons[panelIndex].style.backgroundColor=colorCode;
+    tabButtons[panelIndex].style.color="black";
+    tabPanels.forEach(function(node){
+        node.style.display="none";
+    });
+    tabPanels[panelIndex].style.display="block";
+    tabPanels[panelIndex].style.backgroundColor=colorCode;
 }
-
-for (let th = 0; th < tab_items.length; th++) {
-  document.querySelector('.tab-filter-item-container').style.height =
-    tab_items[th].scrollHeight + 'px';
-}
+showPanel(0,'#F4F4F4');
+showPanel(1,'#F4F4F4');
+showPanel(2,'#F4F4F4');
+showPanel(3,'#F4F4F4');
+showPanel(4,'#F4F4F4');
+showPanel(5,'#F4F4F4');
